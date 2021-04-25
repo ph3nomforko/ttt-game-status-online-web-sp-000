1a635adc96f5b6bc3eb31a0fc59e6f921ca52673
Codi Kraus <codi.kraus@gmail.com>
2021-04-25 20:52:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -46,6 +46,10 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     return true
+  elsif !won?(board) && !full?(board)
+    return false
+  elsif won?(board)
+    return false
   end
 end
 
